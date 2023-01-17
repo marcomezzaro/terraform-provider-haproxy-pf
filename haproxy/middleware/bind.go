5382f21e7704157425400a56894bc755b147e8ed
Marco Mezzaro <marco.mezzaro@elmec.it>
2023-01-17 15:12:06 +0100

    add missing query param in bind and server middleware api

--- haproxy/middleware/bind.go
+++ haproxy/middleware/bind.go
@@ -26,7 +26,7 @@ func (c *Client) GetBinds() (*models.GetBinds, error) {
 
 // return single bind
 func (c *Client) GetBind(bindName string, parentName string) (*models.Bind, error) {
-	url := fmt.Sprintf("%s/services/haproxy/configuration/binds/%s?parent_type=frontend&parent_name=%s", c.base_url, bindName, parentName)
+	url := fmt.Sprintf("%s/services/haproxy/configuration/binds/%s?parent_type=frontend&parent_name=%s&frontend=%s", c.base_url, bindName, parentName, parentName)
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return nil, err
@@ -41,7 +41,7 @@ func (c *Client) GetBind(bindName string, parentName string) (*models.Bind, erro
 }
 
 func (c *Client) CreateBind(transactionId string, bind models.Bind, parentName string) (*models.Bind, error) {
-	url := fmt.Sprintf("%s/services/haproxy/configuration/binds/?parent_type=frontend&parent_name=%s&transaction_id=%s", c.base_url, parentName, transactionId)
+	url := fmt.Sprintf("%s/services/haproxy/configuration/binds/?parent_type=frontend&parent_name=%s&transaction_id=%s&frontend=%s", c.base_url, parentName, transactionId, parentName)
 	bodyStr, _ := json.Marshal(bind)
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(bodyStr))
 	if err != nil {
@@ -59,7 +59,7 @@ func (c *Client) CreateBind(transactionId string, bind models.Bind, parentName s
 }
 
 func (c *Client) UpdateBind(transactionId string, bind models.Bind, parentName string) (*models.Bind, error) {
-	url := fmt.Sprintf("%s/services/haproxy/configuration/binds/%s?transaction_id=%s&parent_type=frontend&parent_name=%s", c.base_url, bind.Name, transactionId, parentName)
+	url := fmt.Sprintf("%s/services/haproxy/configuration/binds/%s?transaction_id=%s&parent_type=frontend&parent_name=%s&frontend=%s", c.base_url, bind.Name, transactionId, parentName, parentName)
 	bodyStr, _ := json.Marshal(bind)
 	req, err := http.NewRequest("PUT", url, bytes.NewBuffer(bodyStr))
 	if err != nil {
@@ -77,7 +77,7 @@ func (c *Client) UpdateBind(transactionId string, bind models.Bind, parentName s
 }
 
 func (c *Client) DeleteBind(transactionId string, bindName string, parentName string) error {
-	url := fmt.Sprintf("%s/services/haproxy/configuration/binds/%s?transaction_id=%s&parent_type=frontend&parent_name=%s", c.base_url, bindName, transactionId, parentName)
+	url := fmt.Sprintf("%s/services/haproxy/configuration/binds/%s?transaction_id=%s&parent_type=frontend&parent_name=%s&frontend=%s", c.base_url, bindName, transactionId, parentName, parentName)
 	req, err := http.NewRequest("DELETE", url, nil)
 	if err != nil {
 		return err
--- haproxy/middleware/server.go
+++ haproxy/middleware/server.go
@@ -26,7 +26,7 @@ func (c *Client) GetServers() (*models.GetServers, error) {
 
 // return single server
 func (c *Client) GetServer(serverName string, parentName string) (*models.Server, error) {
-	url := fmt.Sprintf("%s/services/haproxy/configuration/servers/%s?parent_type=backend&parent_name=%s", c.base_url, serverName, parentName)
+	url := fmt.Sprintf("%s/services/haproxy/configuration/servers/%s?parent_type=backend&parent_name=%s&backend=%s", c.base_url, serverName, parentName, parentName)
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return nil, err
@@ -41,7 +41,7 @@ func (c *Client) GetServer(serverName string, parentName string) (*models.Server
 }
 
 func (c *Client) CreateServer(transactionId string, server models.Server, parentName string) (*models.Server, error) {
-	url := fmt.Sprintf("%s/services/haproxy/configuration/servers/?parent_type=backend&parent_name=%s&transaction_id=%s", c.base_url, parentName, transactionId)
+	url := fmt.Sprintf("%s/services/haproxy/configuration/servers/?parent_type=backend&parent_name=%s&transaction_id=%s&backend=%s", c.base_url, parentName, transactionId, parentName)
 	bodyStr, _ := json.Marshal(server)
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(bodyStr))
 	if err != nil {
@@ -59,7 +59,7 @@ func (c *Client) CreateServer(transactionId string, server models.Server, parent
 }
 
 func (c *Client) UpdateServer(transactionId string, server models.Server, parentName string) (*models.Server, error) {
-	url := fmt.Sprintf("%s/services/haproxy/configuration/servers/%s?transaction_id=%s&parent_type=backend&parent_name=%s", c.base_url, server.Name, transactionId, parentName)
+	url := fmt.Sprintf("%s/services/haproxy/configuration/servers/%s?transaction_id=%s&parent_type=backend&parent_name=%s&backend=%s", c.base_url, server.Name, transactionId, parentName, parentName)
 	bodyStr, _ := json.Marshal(server)
 	req, err := http.NewRequest("PUT", url, bytes.NewBuffer(bodyStr))
 	if err != nil {
@@ -77,7 +77,7 @@ func (c *Client) UpdateServer(transactionId string, server models.Server, parent
 }
 
 func (c *Client) DeleteServer(transactionId string, serverName string, parentName string) error {
-	url := fmt.Sprintf("%s/services/haproxy/configuration/servers/%s?transaction_id=%s&parent_type=backend&parent_name=%s", c.base_url, serverName, transactionId, parentName)
+	url := fmt.Sprintf("%s/services/haproxy/configuration/servers/%s?transaction_id=%s&parent_type=backend&parent_name=%s&backend=%s", c.base_url, serverName, transactionId, parentName, parentName)
 	req, err := http.NewRequest("DELETE", url, nil)
 	if err != nil {
 		return err
